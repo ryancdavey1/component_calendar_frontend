68701e9abd939e4e22b89e8aff9d40f4bfdf70af
ryancdavey1 <ryancdavey1@gmail.com>
2021-01-06 09:22:03 -0700

    add task class for render

---
+++ src/task.js
@@ -0,0 +1,28 @@
+class Task {
+
+  static allTasks = [];
+
+  constructor(task, taskAttributes) {
+    //debugger
+    this.id = task.id;
+    this.name = taskAttributes.name;
+    this.description = taskAttributes.description;
+    this.hours = taskAttributes.hours;
+    this.completed_status = taskAttributes.completed_status;
+    this.initiative = taskAttributes.initiative;
+    Task.allTasks.push(this);
+  }
+
+  renderTask() {
+    return `
+    <div data-id=${this.id} class="box">
+      <h3>${this.name}</h3>
+      <p>${this.description}</p>
+      <p>${this.hours}</p>
+      <p>${this.completed_status}</p>
+      <p>${this.initiative.name}</p>
+      <button data-id=${this.id}>Delete</button>
+    </div>
+    <br><br>`;;
+  }
+}
